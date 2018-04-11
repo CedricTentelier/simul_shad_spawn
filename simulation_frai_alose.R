b2b2d97942367db93ea8ff13a2ea3534185b6bda
Cedric Tentelier <cedric.tentelier@univ-pau.fr>
2018-04-11 07:25:37 +0200

    Nouvel indicateur : nombre maximal de bulls en une nuit

--- simulation_frai_alose.R
+++ simulation_frai_alose.R
@@ -20,34 +20,38 @@ rm(list=ls())
 # Splashes occur in random places (identical within volley), drawn in a Gaussian centered on the central place, with a standard deviation "dispersal". 
 # The function returns a data frame with one line per splash, indicating time and place.
 simul.bull<-function(nfemales=10,lambda.nbulls=12.5,first.day=20170524,mu.days=3.9,theta.days=2.32,
-                     p.isolated.bull=0.2,rest.days=4,mean.hour=82800, sd.hour=3600, nplaces=10,
+                     p.isolated.bull=0.2,rest.days=4,mean.hour=82800, sd.hour=3600, nplaces=2,
                      volley.shape=1.0492316876,volley.rate=0.0014073256,dispersal=1){
-  full.data= data.frame(matrix(vector(), 0, 5, dimnames=list(c(), c("place.bulls","date.bulls","hour.bulls","time.bulls","std.time.bulls"))),stringsAsFactors=F)
+  #Draw a random number of splashes for each female
+  nbulls<-rpois(nfemales,lambda.nbulls)
+  #Draw a random starting date from a negative binomial
+  start.spawn<-ymd(first.day)+days(rnegbin(nfemales,mu=mu.days,theta=theta.days)) 
+  central.place<-round(runif(nfemales,min=1,max=nplaces))
+  #full.data= data.frame(matrix(data=NA, nrow=sum(nbulls), ncol=5, dimnames=list(c(), c("place.bulls","date.bulls","hour.bulls","time.bulls","std.time.bulls"))),stringsAsFactors=F)
+  place.bulls<-numeric(length=sum(nbulls))
+  date.bulls<-ymd(rep('2017-05-27',length=sum(nbulls)))
+  hour.bulls<-hms(rep('10:00:00',length=sum(nbulls)))
   for(f in 1:nfemales){
-    nbulls<-rpois(1,lambda.nbulls)
-    #Draw a random starting date from a negative binomial
-    start.spawn<-ymd(first.day)+days(rnegbin(1,mu=mu.days,theta=theta.days))  
     # Splashes are performed at random nights, with a lag drawn in a Poisson distribution
     # Occurrence within a volley is drawn in a Bernoulli distribution
-    date.bulls<-start.spawn+cumsum(ifelse(rbinom(nbulls,1,p.isolated.bull),rpois(nbulls,lambda=rest.days),0))  
+    date.bulls[(cumsum(nbulls)[f]-nbulls[f]+1):cumsum(nbulls)[f]]<-start.spawn[f]+days(cumsum(ifelse(rbinom(nbulls[f],1,p.isolated.bull),rpois(nbulls[f],lambda=rest.days),0)))  
     # The hour of the first splash of the night is drawn from the Cassou-Leins Normal distribution
+    hour.bulls[(cumsum(nbulls)[f]-nbulls[f]+1):cumsum(nbulls)[f]]<-seconds(round(rnorm(nbulls[f],mean.hour,sd.hour)))
     # The place of the first splash of the night is drawn from a truncated Normal centered around a central place
-    hour.bulls<-seconds(round(rnorm(nbulls,mean.hour,sd.hour)))
-    central.place<-round(runif(1,1,nplaces))
-    place.bulls<-round(rtnorm(nbulls,mean=central.place,sd=dispersal,lower=1,upper=nplaces))
+    place.bulls[(cumsum(nbulls)[f]-nbulls[f]+1):cumsum(nbulls)[f]]<-round(rtnorm(nbulls[f],mean=central.place[f],sd=dispersal,lower=1,upper=nplaces))
     # The delay to other splashes in a volley is drawn from a Gamma distribution
     # All splashes of a volley occur in the same place
-     for(i in 2:length(hour.bulls)){
+     for(i in (cumsum(nbulls)[f]-nbulls[f]+2):cumsum(nbulls)[f]){
       if(date.bulls[i]==date.bulls[i-1]){ 
         hour.bulls[i]<-hour.bulls[i-1]+seconds(round(rgamma(1,volley.shape,volley.rate)))
         place.bulls[i]<-place.bulls[i-1]
       }
-    }
-    time.bulls<-date.bulls+hour.bulls
-    tz(time.bulls)<-"Europe/Paris"
-    std.time.bulls<-ymd(20160501)+hour.bulls
-    full.data<-rbind(full.data,data.frame(place.bulls,date.bulls,hour.bulls,time.bulls,std.time.bulls))
-  }  
+     }
+  } 
+  time.bulls<-date.bulls+hour.bulls
+  tz(time.bulls)<-"Europe/Paris"
+  std.time.bulls<-ymd(20160501)+hour.bulls
+  full.data<-data.frame(place.bulls,date.bulls,hour.bulls,time.bulls,std.time.bulls)
   return(full.data)
 }
 ############################################################################################
@@ -58,12 +62,13 @@ simul.bull<-function(nfemales=10,lambda.nbulls=12.5,first.day=20170524,mu.days=3
 ######################################################################################################
 # This function uses a dataset with 1 line per splash (with its date, hour and place)
 # to plot 3 distributions of the number of splashes 1) per spawning ground, 2) per day, 3) per hour accross days
-distr.plot<-function(data){
+summary.bull<-function(data){
   windows()
   par(mfrow=c(1,3))
   hist(data$place.bulls,freq=T,main="Nb bulls/frayere",xlab="Nb de frayere")
   hist(data$date.bulls,freq=T,breaks=30, main="Saison sur toutes frayeres",xlab="Date")
   hist(data$std.time.bulls,freq=T,breaks=20, main="Distribution horaire",xlab="Heure")
+  observed.nbulls<-nrow(data)
 }
 #######################################################################################################
 # This function takes a full record of all splashes occurring in a river (all.splashes) and a sampling plan (ech)
@@ -71,9 +76,9 @@ distr.plot<-function(data){
 # all.splashes is a data frame with one row per splash, with its place and time
 # ech is a data frame with 3 variables: site number, starting time and ending time of the session
 sampling<-function(all.splashes,ech){
-  if(missing(ech)){
+  #if(missing(ech)){
     #build the sampling plan using start.date, stop.date, n.sites, sampling.intensity, sampling.skew
-  }
+  #}
   sampled.data<-subset(all.splashes,subset=as.logical(colSums(sapply(all.splashes$time.bulls, '%within%', interval(dmy_hms(ech$start,tz="Europe/Paris"),dmy_hms(ech$end,tz="Europe/Paris")),simplify=T)*sapply(all.splashes$place.bulls,'==',ech$site))))
   return(sampled.data)
 }
@@ -89,18 +94,20 @@ sampling<-function(all.splashes,ech){
 #######################
 ### MAIN CODE ####
 #######################
-sampling.plan<-read.csv("G:/Mes Documents/St_Pee/Alose/Simulations/fictional sampling.csv",header=T,sep=";")
+sampling.plan<-read.csv("G:/Mes Documents/St_Pee/Alose/Simulations/fictional sampling2.csv",header=T,sep=";")
 actual.nfemales=NULL
 actual.nbulls=NULL
 observed.nbulls=NULL
-for(r in 1:100){
+max.in.1.night=NULL
+for(r in 1:20){
  actual.nfemales[r]<-floor(rnorm(1,100,20))
  full.data<-simul.bull(nfemales=actual.nfemales[r],nplaces = 2)
  actual.nbulls[r]<-nrow(full.data)
  observed.data<-sampling(full.data,sampling.plan)
- observed.nbulls[r]<-nrow(observed.data)
+ observed.nbulls[r]<-nrow(observed.data) #total number of splashes observed in the season
+ max.in.1.night[r]<-max(tabulate(yday(observed.data$time.bulls+hours(12)))) #maximum number of splashes observed in a single night
 }
 windows()
-plot(as.data.frame(cbind(actual.nfemales,actual.nbulls,observed.nbulls)))
+plot(as.data.frame(cbind(actual.nfemales,actual.nbulls,observed.nbulls,max.in.1.night)))
 
 
